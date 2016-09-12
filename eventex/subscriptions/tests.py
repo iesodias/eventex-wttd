1291e9b7863dc3cce1330301723adedfb979154e
iesodias <iesodias@gmail.com>
2016-09-12 17:01:59 -0300

    Tela de Inscricao

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,7 +1,34 @@
 from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
 
 class SubscribeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
     def test_get(self):
         """"Get /incricao/ must return status code 200"""
-        response = self.client.get('/inscricao')
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        """" must use subscription/subscription_form.html"""
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
+
+    def test_html(self):
+        self.assertContains(self.resp, '<form')
+        self.assertContains(self.resp, '<input', 6)
+        self.assertContains(self.resp, 'type="text"', 3)
+        self.assertContains(self.resp, 'type="email"')
+        self.assertContains(self.resp, 'type="submit"')
+
+    def teste_csrf(self):
+        """Html must contain csrf"""
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
+
+    def test_has_form(self):
+        """ Context must have subscription form"""
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_filds(self):
+        """Form must have 4 filds"""
+        form = self.resp.context['form']
+        self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
