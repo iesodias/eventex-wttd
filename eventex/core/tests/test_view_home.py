e035a91402fa5fad152d2cdd2350c264b090d9a8
iesodias <iesodias@gmail.com>
2016-09-23 14:35:18 -0300

    urls.py

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
 class HomeTest(TestCase):
     def setUp(self):
-        self.response = self.client.get('/')
+        self.response = self.client.get(r('home'))
 
     def test_get(self):
         self.assertEqual(200, self.response.status_code)
